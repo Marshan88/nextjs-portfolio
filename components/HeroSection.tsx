bf8fc090fc273e223c982e79a15c0e2bc67aa885
Marshan88 <92783803+Marshan88@users.noreply.github.com>
2023-05-31 16:09:39 +0200

    Update

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,30 +6,6 @@ import { Link } from "react-scroll/modules";
 import { HiArrowDown } from "react-icons/hi";
 import { Container, Row, Col } from "react-bootstrap";
 
-/*
-.banner .tagline {
-  font-weight: 700;
-  letter-spacing: 0.8px;
-  padding: 8px 10px;
-  background: linear-gradient(90.21deg, black -5.91%, #2d2d2d 111.58%);
-  border: 1px solid #ffdd19;
-  font-size: 20px;
-  margin-bottom: 16px;
-  display: inline-block;
-*/
-const skills = [
-  { skill: "HTML" },
-  { skill: "CSS" },
-  { skill: "JavaScript" },
-  { skill: "Tailwind CSS" },
-  { skill: "TypeScript" },
-  { skill: "React" },
-  { skill: "Next.js" },
-  { skill: "GitHub" },
-  { skill: ".NET framework" },
-  { skill: "SQL" },
-];
-
 const HeroSection = () => {
   const el = React.useRef(null);
   React.useEffect(() => {
@@ -49,6 +25,7 @@ const HeroSection = () => {
   return (
     <section
       id="home"
+      className="h-screen"
       // className="h-screen px-3.5 bg-[url('/BG.jpg')] bg-top-center bg-no-repeat bg-fixed bg-auto bg-black overflow-hidden"
     >
       <div className="flex flex-col text-center items-center justify-center my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
@@ -69,46 +46,16 @@ const HeroSection = () => {
               </h2>
               <br></br>
               <p className="text-xl">
-                I&#39;m Marius, I like programming, tech and competative online
-                games.
-                <br></br>
-                Did a course in front-end and back-end at{" "}
-                <a
-                  className="text-green-300"
-                  href="https://www.getacademy.no/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GET-Academy
-                </a>
-                ,<br></br>
-                currently looking for work/internship.<br></br>
-                <br></br>
-              </p>
-              <div className="text-center md:w-1/2 md:text-left">
-                <h1 className="text-3xl font-bold mb-4 text-pink-500">
-                  My Skills
-                </h1>
-                <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
-                  {skills.map((item, idx) => {
-                    return (
-                      <p
-                        key={idx}
-                        className="bg-gray-800 px-4 py-2 mr-2 mt-2 text-pink-500 rounded font-bold"
-                      >
-                        {item.skill}
-                      </p>
-                    );
-                  })}
-                </div>
-              </div>
+                My name is Marius, an IT-Developer based in Tønsberg, Norway.
+                Currently looking for work/internship.
+              </p>{" "}
             </Col>
           </Row>
         </Container>
         <Col>
           <div className="float-right">
             <Image
-              className="float-right contrast-200 brightness-200"
+              className="contrast-200 brightness-200"
               src="/logo.png"
               alt=""
               width={600}
@@ -127,7 +74,10 @@ const HeroSection = () => {
           offset={-100}
           duration={500}
         >
-          <HiArrowDown size={35} className="animate-bounce" />
+          <HiArrowDown
+            size={35}
+            className="animate-bounce cursor-pointer hover:text-green-300"
+          />
         </Link>
       </div>
     </section>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,13 +32,13 @@ export default function Navbar() {
   const pathname = usePathname();
   const [navbar, setNavbar] = useState(false);
   return (
-    <header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
+    <header className=" py-0 sm:py-0 md:py-0 md:flex-row md:space-x-4 w-full mx-auto px-4 sm:px-10 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <Link to="home">
               <div className="container flex items-center space-x-2">
-                <h2 className="text-2xl font-bold">Marius</h2>
+                <h2 className="text-2xl font-bold cursor-pointer">Marius</h2>
               </div>
             </Link>
             <div className="md:hidden">
@@ -64,7 +64,7 @@ export default function Navbar() {
                     key={idx}
                     to={item.page}
                     className={
-                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                      "cursor-pointer block lg:inline-block text-neutral-900  hover:text-green-300 dark:text-neutral-100"
                     }
                     activeClass="active"
                     spy={true}
@@ -80,14 +80,14 @@ export default function Navbar() {
               {currentTheme === "dark" ? (
                 <button
                   onClick={() => setTheme("light")}
-                  className="bg-slate-100 p-2 rounded-xl"
+                  className="bg-slate-100 p-2 rounded-xl hover:bg-yellow-300"
                 >
                   <RiSunLine size={25} color="black" />
                 </button>
               ) : (
                 <button
                   onClick={() => setTheme("dark")}
-                  className="bg-slate-100 p-2 rounded-xl"
+                  className="bg-slate-100 p-2 rounded-xl hover:bg-blue-300"
                 >
                   <RiMoonFill size={25} color="black" />
                 </button>

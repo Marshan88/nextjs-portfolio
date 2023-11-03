0dd6050f91200749e241ea5e6e9c350cc77a70fc
Marshan88 <92783803+Marshan88@users.noreply.github.com>
2023-11-03 17:55:18 +0100

    Revert "builderio"
    This reverts commit 05b14e217cfd0aac589848ae7e2d90ec5a49b424.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const withBuilderDevTools = require("@builder.io/dev-tools/next")();
-
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = withBuilderDevTools({
+const nextConfig = {
   /* config options here */
-});
+};
 
 module.exports = nextConfig;

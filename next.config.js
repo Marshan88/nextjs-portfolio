05b14e217cfd0aac589848ae7e2d90ec5a49b424
Marshan88 <92783803+Marshan88@users.noreply.github.com>
2023-11-03 16:55:20 +0100

    builderio

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const withBuilderDevTools = require("@builder.io/dev-tools/next")();
+
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
+const nextConfig = withBuilderDevTools({
   /* config options here */
-};
+});
 
 module.exports = nextConfig;
